4d7d4da3a40a6adc819e868ad9bb86ff15b75ab8
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-11-08 13:23:45 +0100

    feat: init API

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-val scala3Version = "3.7.3"
+val scala3Version = "3.7.4"
 
 lazy val root = project
   .in(file("."))
@@ -8,5 +8,11 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "1.2.1" % Test,
+      "org.http4s" %% "http4s-ember-server" % "0.23.33",
+      "org.http4s" %% "http4s-dsl" % "0.23.33",
+      "com.softwaremill.sttp.tapir" %% "tapir-core" % "1.12.2",
+      "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "1.12.2"
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,33 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import cats.effect.*
+import org.http4s.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Router
+import com.comcast.ip4s.*
+import sttp.tapir.*
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-def msg = "I was compiled by Scala 3. :)"
+object Main extends IOApp:
+
+  // Définition de l'endpoint Tapir pour /api
+  val helloEndpoint = endpoint
+    .get
+    .in("api")
+    .out(stringBody)
+    .serverLogicSuccess[IO](_ => IO.pure("Hello, World!"))
+
+  // Conversion de l'endpoint Tapir en routes http4s
+  val routes = Http4sServerInterpreter[IO]()
+    .toRoutes(helloEndpoint)
+
+  val httpApp = Router("/" -> routes).orNotFound
+
+  // Démarrage du serveur
+  def run(args: List[String]): IO[ExitCode] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(httpApp)
+      .build
+      .use(_ => IO.println("Serveur démarré sur http://localhost:8080/api") *> IO.never)
+      .as(ExitCode.Success)
